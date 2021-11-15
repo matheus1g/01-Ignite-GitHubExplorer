677a5fcd7be1da07735fef5ad1ffa2fcc2f71c6f
Matheus Tosta <matheusgomestosta8@gmail.com>
2021-11-15 18:34:03 -0300

    adicionando busca de usuario

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -9,21 +9,39 @@ interface Repository {
   html_url: string;
 }
 
-
 export function RepositoryList() {
 
   const [repositories, setRepositories] = useState<Repository[]>([]);
 
+  const [user,setUser] = useState('matheus1g');
+  const [nick,setNick] = useState('');
+
   useEffect(() => {
-    fetch('https://api.github.com/orgs/rocketseat/repos')
-      .then(response => response.json())
+     fetch(`https://api.github.com/users/${user}/repos`)
+      .then(response => response.json())     
       .then(data => setRepositories(data))
-  },[])
+
+  },[user])
+
+  function handleClick(nick:string) {
+    if (!nick) return (alert("Preencha o usuario"))
+    
+    setUser(nick);
+    setNick('');
+  }
 
   return(
     <section className="repository-list">
-      <h1>Lista de repositório</h1>
-
+      <h1>Lista de repositório - {user}</h1>
+      <input
+      type="text"
+      placeholder="Digite o usuario"
+      onChange={(e) => setNick(e.target.value)}
+      value={nick}
+      />
+      <button onClick={() => handleClick(nick)} >
+      Buscar
+      </button>
       <ul>
         {repositories.map(repository => {
           return <RepositoryItem key={repository.name} repository={repository} />
